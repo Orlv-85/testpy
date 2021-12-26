d78ca7ce898ede8b67c1183601ededf5707d7bfc
Orlv-85 <48240603+Orlv-85@users.noreply.github.com>
2021-12-26 18:14:48 +0300

    Add files via upload

--- test_webtest.py
+++ test_webtest.py
@@ -1,10 +1,28 @@
 import pytest
-import ClientFlasktest
+import requests
 
-gweb=Get_webtest()
+class Get_webtest:
+
+     def get_users (self,a,b):
+         resp=requests.get('http://127.0.0.1:5000/users', {a:b})
+         return resp
+gu=Get_webtest()
 
 @pytest.fixture()
-gweb.get_users(0,0)
+def get_users1():
+    resp=gu.get_users ('username','ch')
+    return resp
 
-def test_response(get_users):
-        assert get_users == '<Response [200]>'
+@pytest.fixture()
+def get_users2():
+    resp=gu.get_users ('departament','Dvarf')
+    return resp
+
+def test_response1(get_users1):
+    assert str(get_users1) == '<Response [200]>'
+
+def test_response2(get_users1):
+    assert str(get_users1.text) == 'vorchun veselchak chihun'
+
+def test_response3(get_users2):
+    assert str(get_users2.text) == 'umnik veselchak skromink prostak'
